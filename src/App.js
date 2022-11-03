bdd393e94c5b8d9de67cd29fa61a9a7fa446b8e7
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-03 14:50:18 -0600

    Create folder for components; add to app file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <h1>Waffle</h1>
+      </nav>
+      <Board />
+      <Keyboard />
     </div>
   );
 }
