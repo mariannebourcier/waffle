8c66f217d9edeeb354977bed764b0151e0147fc7
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-03 16:03:43 -0600

    Create key component for keyboard and add to keyboard file

---
+++ src/components/Key.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Key({ keyValue }) {
+  return (
+    <div className='key'>{keyValue}</div>
+  )
+}
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import Key from './Key'
 
 export default function Keyboard() {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "x", "C", "V", "B", "N", "M"];
-  
+
   return (
     <div className="keyboard">
       <div className="line1">
-        
+        {keys1.map((key) => {
+          return <Key keyValue={key}/>
+        })}
       </div>
-      <div className="line2"></div>
-      <div className="line3"></div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key keyValue={key}/>
+        })}
+      </div>
+      <div className="line3">
+        {keys3.map((key) => {
+          return <Key keyValue={key}/>
+        })}
+      </div>
 
     </div>
   )
