95fda9974eae2e5ce8227fd47477a03a6ce0452d
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-03 15:57:17 -0600

    Add letters to keyboard component

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function Keyboard() {
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "x", "C", "V", "B", "N", "M"];
+  
   return (
     <div className="keyboard">
       <div className="line1">
