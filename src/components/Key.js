9d5f4afa3b447fc91ab305fce0996d9f4686f483
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-06 14:46:42 -0700

    Add function to choose letter in board

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+
 
 export default function Key({ keyValue, bigKey }) {
+  const { board, setBoard } = useContext(AppContext) 
+  const selectLetter = () => {
+    const currentBoard = [...board]
+    currentBoard[0][0] = keyValue
+    setBoard(currentBoard)
+  }
   return (
-    <div className='key' id={bigKey && "big"}>
+    <div className='key' id={bigKey && "big"} onClick={selectLetter}>
       {keyValue}
     </div>
   )
