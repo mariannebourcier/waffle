e7e34cac89105980c7882bb1c7aff12042e77988
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-07 15:44:06 -0700

    Add function to keep track of attempts

--- src/components/Key.js
+++ src/components/Key.js
@@ -5,6 +5,12 @@ import { AppContext } from '../App';
 export default function Key({ keyValue, bigKey }) {
   const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext) 
   const selectLetter = () => {
+    if (keyValue === "ENTER") {
+      if (currentAttempt.letterPos !== 5) return;
+      setCurrentAttempt({attempt: currentAttempt.attempt + 1 })
+    } else {
+
+  
     //check to change line when word done
     if (currentAttempt.letterPos > 4) return; 
     const currentBoard = [...board]
@@ -12,6 +18,7 @@ export default function Key({ keyValue, bigKey }) {
     setBoard(currentBoard)
     setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1 })
   }
+}
   return (
     <div className='key' id={bigKey && "big"} onClick={selectLetter}>
       {keyValue}
