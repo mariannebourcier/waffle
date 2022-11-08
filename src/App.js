856be1e358e6ba53bb28eb1c13456519c1a1a35b
mariannebourcier <marianne.bourcier@gmail.com>
2022-11-08 15:21:01 -0700

    Move functions inside app context

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ export default function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currentAttempt, setCurrentAttempt] = useState({ attempt: 0, letterPos: 0 })
 
+  //helper functions
+  const onSelectLetter = (keyValue) => {
+    if (currentAttempt.letterPos > 4) return;
+    const currentBoard = [...board]
+    currentBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyValue
+    setBoard(currentBoard)
+    setCurrentAttempt({ ...currentAttempt, letterPos: currentAttempt.letterPos + 1 })
+  }
+
+  const onDelete = () => {
+    if (currentAttempt.letterPos === 0) return;
+    const currentBoard = [...board];
+    currentBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(currentBoard)
+    setCurrentAttempt({ ...currentAttempt, letterPos: currentAttempt.letterPos - 1 })
+  }
+
+  const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) return;
+    setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0 })
+  }
+
+
   return (
     <div className="App">
       <nav>
@@ -20,7 +43,10 @@ export default function App() {
         board,
         setBoard,
         currentAttempt,
-        setCurrentAttempt
+        setCurrentAttempt,
+        onSelectLetter,
+        onDelete,
+        onEnter
          }}>
       <Board />
       <Keyboard />
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,35 +1,30 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../App';
-import SelectLetter from './helpers/SelectLetter';
 
 
 export default function Key({ keyValue, bigKey }) {
-//   const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext) 
-//   const selectLetter = () => {
-//     //new line for attempt
-//     if (keyValue === "ENTER") {
-//       if (currentAttempt.letterPos !== 5) return;
-//       setCurrentAttempt({attempt: currentAttempt.attempt + 1, letterPos: 0 })
-//     } //delete last letter
-//     else if (keyValue === "DELETE") {
-//       if (currentAttempt.letterPos === 0) return;
-//       const currentBoard = [...board];
-//       currentBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
-//       setBoard(currentBoard)
-//       setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos - 1})
-//     } else {
+   const {
+    board,
+    setBoard, 
+    currentAttempt, 
+    setCurrentAttempt,
+    onDelete,
+    onEnter,
+    onSelectLetter 
+  } = useContext(AppContext) 
 
-  
-//     //check to change line when word done
-//     if (currentAttempt.letterPos > 4) return; 
-//     const currentBoard = [...board]
-//     currentBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyValue
-//     setBoard(currentBoard)
-//     setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos + 1 })
-//   }
-// }
+  const selectLetter = () => {
+    if (keyValue === "ENTER") {
+      onEnter()
+    } else if (keyValue === "DELETE") {
+      onDelete()
+    } else {
+      onSelectLetter(keyValue)
+    }
+  }
+
   return (
-    <div className='key' id={bigKey && "big"} onClick={SelectLetter}>
+    <div className='key' id={bigKey && "big"} onClick={selectLetter}>
       {keyValue}
     </div>
   )
